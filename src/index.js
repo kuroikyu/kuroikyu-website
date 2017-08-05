5686ad5cb475ab6c23314b9bcf9ffd3cb7d387d6
kuroikyu <kuroikyu+git@gmail.com>
2017-08-05 21:49:20 +0100

    ✨ Add smooth scrolling for Projects link

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './sass/main.sass'
 import 'font-awesome/css/font-awesome.css'
 import 'normalize.css'
+
+// Smooth scroll-to
+const projectsButton = document.querySelector('.scroll-to-projects');
+const projectsArea = document.querySelector('#projects');
+
+projectsButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  projectsArea.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
