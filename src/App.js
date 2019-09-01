a6ca158af85bfcb0931e14b2a7e1ef50817b9c5c
kuroikyu <kuroikyu+github@gmail.com>
2019-09-01 12:50:22 +0100

    Update coding style to match the rest of the project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Landing from './components/Landing';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
-import './App.css';
+import Landing from './components/Landing'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+import './App.css'
 import projectsData from './projects.json'
 
 class App extends Component {
   state = {
     projects: [],
     windowWidth: 756,
-  };
+  }
 
   async componentDidMount() {
     // Reinstate data from localStorage
-    const localStorageData = localStorage.getItem('kuroikyu.com-state');
+    const localStorageData = localStorage.getItem('kuroikyu.com-state')
     if (localStorageData) {
-      this.setState(JSON.parse(localStorageData));
+      this.setState(JSON.parse(localStorageData))
     }
 
     // Fetch projects from API
-    const windowWidth = window.innerWidth;
-    this.setState({ projects, windowWidth });
     const projects = projectsData
+    const windowWidth = window.innerWidth
+    this.setState({ projects, windowWidth })
   }
 
   componentDidUpdate() {
-    const state = JSON.stringify(this.state);
-    localStorage.setItem('kuroikyu.com-state', state);
+    const state = JSON.stringify(this.state)
+    localStorage.setItem('kuroikyu.com-state', state)
   }
 
   render() {
-    const { projects, windowWidth } = this.state;
+    const { projects, windowWidth } = this.state
     return (
       <div className="App">
         <Landing windowWidth={windowWidth} />
         <Projects projects={projects} windowWidth={windowWidth} />
         <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
